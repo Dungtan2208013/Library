b880df67d732bcc0c6142a3dbc2ee275c6ae92e7
DungTan <dungptth2208013@fpt.edu.vn>
2023-05-27 00:38:54 +0700

    .
    .

---
+++ Library.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise1
+{
+    public class libary
+    {
+        public string Title;
+        public string Author;
+        public int Year;
+
+        public override string ToString()
+        {
+            return "Type: Library Item\nTitle: " + Title + "\nAuthor: " + Author + "\nYear: " + Year + "\n";
+        }
+
+    }
+    public class Book : libary
+    {
+        public override string ToString()
+        {
+            return "Type: Book\nTitle: " + Title + "\nAuthor: " + Author + "\nYear: " + Year + "\n";
+        }
+    }
+    public class Magazine : libary
+    {
+        public override string ToString()
+        {
+            return "Type: Magazine\nTitle: " + Title + "\nAuthor: " + Author + "\nYear: " + Year + "\n";
+        }
+    }
+
+    public class DVD : libary
+    {
+        public override string ToString()
+        {
+            return "Type: DVD\nTitle: " + Title + "\nAuthor: " + Author + "\nYear: " + Year + "\n";
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            book.Title = "C# de ma";
+            book.Author = "Pham Tan Dung";
+            book.Year = 2003;
+            Console.WriteLine(book.ToString());
+
+
+            Magazine magazine = new Magazine();
+            magazine.Title = "A cay oi";
+            magazine.Author = "Alu";
+            magazine.Year = 2000;
+            Console.WriteLine(magazine.ToString());
+
+            DVD dvd = new DVD();
+            dvd.Title = "thien online";
+            dvd.Author = "style";
+            dvd.Year = 2021;
+            Console.WriteLine(dvd.ToString());
+
+
+            Console.ReadLine();
+        }
+    }
+}
